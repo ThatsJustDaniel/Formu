8bcacd899f59e60ed3c78eea24c6dc3477b35c23
Daniel Ortiz <danielo95@live.com>
2022-06-03 15:46:20 -0500

    Initial Commit

---
+++ Formu WatchKit Extension/FormuApp.swift
@@ -0,0 +1,19 @@
+//
+//  FormuApp.swift
+//  Formu WatchKit Extension
+//
+//  Created by Daniel Ortiz on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FormuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ Formu/FormuApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormuApp.swift
+//  Formu
+//
+//  Created by Daniel Ortiz on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct FormuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
